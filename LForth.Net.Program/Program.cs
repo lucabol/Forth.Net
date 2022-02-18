86bc9859ea2f4c95ebf383ac042f9ebe59634854
Luca Bolognese <lucabol@microsoft.com>
2022-02-18 09:43:35 +0100

    Move to new tokenized architecture

---
+++ LForth.Net.Program/Program.cs
@@ -0,0 +1,143 @@
+﻿using Forth;
+using static System.Console;
+
+using CommandLine;
+using CommandLine.Text;
+
+var verbose      = false;
+Options? options = null;
+
+Vm vm = new();
+var parser       = new CommandLine.Parser(with => with.HelpWriter = null);
+var parserResult = parser.ParseArguments<Options>(args);
+parserResult
+    .WithParsed<Options>(options => Run(options))
+    .WithNotParsed(errs => DisplayHelp(parserResult, errs));
+
+void Run(Options o) {
+
+    options = o;
+    verbose = options.Verbose;
+
+    foreach (var fileName in o.Files)
+        InterpretFile(fileName);
+
+    if(o.Exec is not null)
+        vm.EvaluateSingleLine(o.Exec);
+
+    WriteLine("LForth.Net by Luca Bolognese (2022)");
+    WriteLine("Say 'bye' to exit. 'debug' to see more. The rest is Forth.\n");
+    System.ReadLine.HistoryEnabled = true;
+
+    vm.NextLine = NextLine;
+    while(true)
+        try
+        {
+            vm.Quit();
+        } catch(ForthException e) { 
+            ColorLine(ConsoleColor.Red, e.Message);
+            vm.Reset();
+        } catch(ArgumentOutOfRangeException e) {
+            if(e.StackTrace?.ToString().Contains("Pop") is not null)
+                ColorLine(ConsoleColor.Red, "Possible stack underflow (it is expensive to check). Enable 'debug' to see the full exception.");
+            if(vm.Debug) ColorLine(ConsoleColor.Gray, e.ToString());
+            vm.Reset();
+        } catch(Exception e)
+        {
+            ColorLine(ConsoleColor.Red, e.Message + " Enable 'debug' to see the full exception.");
+            if(vm.Debug) ColorLine(ConsoleColor.Gray, e.ToString());
+            vm.Reset();
+        }
+}
+
+static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+{
+  var helpText = HelpText.AutoBuild(result, h =>
+  {
+    h.AdditionalNewLineAfterOption = false;
+    h.AddPreOptionsLine("Usage: lforth [Forth files] [Options]");
+    h.AddPostOptionsText
+        ("EXAMPLES:\n\tlforth\n\tlforth Test1.fth Test2.fth -e bye\n\tlforth Test1.fth Test2.fth -o Forth.fim");
+    return HelpText.DefaultParsingErrorsHandler(result, h);
+  }, e => e);
+
+  Console.WriteLine(helpText);
+}
+
+string NextLine() {
+    
+        if(CursorLeft != 0) WriteLine();
+        if(!options.HideStack) ColorLine(ConsoleColor.Gray, vm.DotS());
+
+        System.ReadLine.AutoCompletionHandler = new AutoCompletionHandler(vm);
+
+        CursorVisible = true;
+        var line = System.ReadLine.Read();
+        CursorVisible = false;
+
+        return line;
+};
+
+
+void VWrite(string s)     { if(verbose) Write(s); };
+void VWriteLine(string s) { if(verbose) Write(s); };
+
+void InterpretFile(string fileName)
+{
+
+    var lineNum = 0;
+    var lineText = "";
+
+    try {
+        VWrite($"Interpreting file {fileName} ...");
+        using var stream = File.OpenRead(fileName);
+        using var reader = new StreamReader(stream);
+        vm.NextLine = () => { lineNum++; lineText = reader.ReadLine()! ; return lineText; };
+        vm.Quit();
+        VWriteLine(" done.\n");
+    } catch(Exception)
+    {
+        ColorLine(ConsoleColor.Red, $"File: {fileName} Line: {lineNum}\n{lineText}");
+        throw; 
+    }
+}
+void ColorLine(ConsoleColor color, string s) {
+    var backupcolor = ForegroundColor;
+    ForegroundColor = color;
+    Console.WriteLine(s);
+    ForegroundColor = backupcolor;
+}
+
+class AutoCompletionHandler : IAutoCompleteHandler
+{
+    public char[] Separators { get; set; } = new char[] { ' ' };
+    public IEnumerable<string> words;
+
+    public string[] GetSuggestions(string text, int index)
+    {
+        if(string.IsNullOrWhiteSpace(text)) return words.ToArray();
+
+        return words.Where(
+                s => s.StartsWith(
+                text.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.TrimEntries).Last())
+                ).ToArray();
+    }
+
+    public AutoCompletionHandler(Vm vm) {
+        words = vm.Words();
+    }
+}
+
+class Options {
+    [Option('e', "exec [forthstring]", Required = false, HelpText = "Execute forthstring after starting up.")]
+    public string? Exec {get; set;}
+
+    [Value(0, MetaName="Forth files", HelpText = "Optional Forth files to compile or execute.")]
+    public IEnumerable<string>? Files {get; set;}
+
+    [Option('v', "verbose", Required = false, HelpText = "Produce verbose output.")]
+    public bool Verbose {get; set;} = false;
+
+    [Option('s', "hidestack", Required = false, HelpText = "Hides the stack banner.")]
+    public bool HideStack {get; set;} = false;
+}
---
+++ LForth.Net.Program/init.fth
@@ -0,0 +1,77 @@
+﻿: ( [char] ) parse drop drop ; immediate
+: \ 0 word drop ; immediate
+
+\ Some modified from https://theforth.net/package/minimal/current-view/README.md
+: variable create 0 , ;
+: constant create , does> @ ;
+
+\ Arithmetic
+: 1+ 1 + ;
+: 2+ 2 + ;
+: 1- 1 - ;
+: 2- 2 - ;
+: min ( n1 n2 -- n3 )  over over > if swap then drop ;
+: max ( n1 n2 -- n3 )  over over < if swap then drop ;
+: mod ( n n -- n )  1 swap */mod drop ;
+: dec 10 base ! ;
+: hex 16 base ! ;
+: 2* 2 * ;
+: negate -1 * ;
+: d- - ;
+
+\ Stack
+: rot ( x1 x2 x3 -- x2 x3 x1 )  >r  swap r> swap ;
+: -rot ( x1 x2 x3 -- x3 x2 x1 )  rot rot ;
+: nip ( x1 x2 -- x2 )  swap drop ;
+: tuck ( x1 x2 -- x2 x1 x2 )  swap over ;
+: ?dup dup 0 <> if dup then ;
+: bounds ( addr1 u -- addr2 addr3 )  over + swap ;
+: 2dup ( d1 -- d1 d1 )  over over ;
+: 2swap ( d1 d2 -- d2 d1 )  >r rot rot r> rot rot ;
+: 2over ( d1 d2 -- d1 d2 d1 )  >r >r 2dup r> r> 2swap ;
+: um/mod 2dup mod -rot / ;
+
+\ Boolean
+0 constant false
+false invert constant true
+: 0= 0 = ;
+: 0< 0 < ;
+: 0> 0 > ;
+: or ( x x -- x )  invert swap invert and invert ; ( do morgan )
+: xor ( x x -- x )  over over invert and >r swap invert and r> or ;
+: lshift ( x1 u -- x2 )  begin dup while >r  2*  r> 1 - repeat drop ;
+: endif postpone then ; immediate
+
+\ Memory
+: ? @ . ;
+: +! ( x addr -- )  swap over @ + swap ! ;
+: chars ;
+: char+ ( c-addr1 -- c-addr2 )  1 chars + ;
+: cell+ ( addr1 -- addr2 )  1 cells + ;
+: aligned ( addr -- a-addr )  cell+ 1 -   1 cells 1 - invert  and ;
+: 2! ( d addr -- )   SWAP OVER ! CELL+ ! ;
+: 2@ ( addr -- d )  DUP CELL+ @ SWAP @ ;
+
+\ Compiler
+: ' bl word find drop ;
+: ['] ' postpone literal ; immediate
+: value ( -- )  create , does> @ ;
+: defer ( "<spaces>name" -- )  create 0 , does> @ execute ;
+: to ( x "<spaces>name" -- ) 
+   state @ 
+   if  postpone [']  postpone >body postpone !  
+   else ' >body ! then ; immediate
+
+: is ( x "<spaces>name" -- ) 
+   state @ if  postpone to  else ['] to execute  then ; immediate
+
+\ Strings
+: space ( -- )  bl emit ;
+: spaces ( u -- ) dup 0 > if  begin dup while  space 1 -  repeat  then  drop ;
+
+\ .net inteop samples
+: .net ( type-s-addr type-c methodName-s-addr method-name-c -- ** )
+	2swap .net>type .net>method .net>call ;
+
+: escape s" System.Uri, System" s" EscapeDataString" .net ;
+: sqrt s" System.Math" s" Sqrt" .net ;
