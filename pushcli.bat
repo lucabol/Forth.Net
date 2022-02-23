b35d26be5a7d827ab589db9542489e47ec85443d
Luca Bolognese <lucabol@microsoft.com>
2022-02-23 13:51:16 +0100

    Finally

---
+++ pushcli.bat
@@ -0,0 +1,3 @@
+﻿del Forth.Net.Program\nupkg\*.nupkg
+dotnet pack Forth.Net.Program/Forth.Net.Program.csproj --configuration Release
+dotnet nuget push .\Forth.Net.Program\nupkg\*.nupkg --source https://api.nuget.org/v3/index.json --api-key %NUGET_FORTH%
---
+++ pushlib.bat
@@ -0,0 +1,3 @@
+﻿del Forth.Net\nupkg\*.nupkg
+dotnet pack Forth.Net\Forth.Net.csproj --configuration Release -o Forth.Net\nupkg
+dotnet nuget push .\Forth.Net\nupkg\*.nupkg --source https://api.nuget.org/v3/index.json --api-key %NUGET_FORTH%
